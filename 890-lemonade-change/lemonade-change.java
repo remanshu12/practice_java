078358b778646f83ea61280d94becc3505203ecb
remanshu12 <remanshugoyal1@gmail.com>
2025-10-02 05:20:12 +0530

    Time: 2 ms (93.17%) | Memory: 55.4 MB (97.15%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                five++;
+            }else if(bills[i]==10 && five>0){
+                five--;
+                ten++;
+            }else if(bills[i]==20 ) {
+                if((ten>0 && five>0)){
+                    ten--;
+                    five--;
+                }else if(five>2){
+                    five-=3;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
