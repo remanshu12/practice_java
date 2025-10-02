383d2f531e298b461c53e1408d3c95f190f49035
remanshu12 <remanshugoyal1@gmail.com>
2025-10-02 05:31:00 +0530

    Time: 8 ms (56.39%) | Memory: 43 MB (43.67%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,35 +1,32 @@
-import java.util.*;
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-
-        // sorted version of s1
-        char[] arr = s1.toCharArray();
-        Arrays.sort(arr);
-        String sortedS1 = new String(arr);
-
-        // sliding window of length s1
-        StringBuilder window = new StringBuilder();
-        for (int i = 0; i < s1.length(); i++) {
-            window.append(s2.charAt(i));
+        char arr[]=new char[26];
+        char arr2[]=new char[26];
+        if(s2.length()<s1.length()){
+            return false;
         }
-
-        if (match(sortedS1, window.toString())) return true;
-
-        for (int i = s1.length(); i < s2.length(); i++) {
-            window.deleteCharAt(0);           // remove first char
-            window.append(s2.charAt(i));      // add new char
-
-            if (match(sortedS1, window.toString())) return true;
+        for(int i=0;i<s1.length();i++){
+            arr[s1.charAt(i)-'a']++;
+            arr2[s2.charAt(i)-'a']++;
+        }
+        if(match(arr,arr2)){
+            return true;
+        }
+        for(int i=s1.length();i<s2.length();i++){
+            arr2[s2.charAt(i)-'a']++;
+            arr2[s2.charAt(i-s1.length())-'a']--;
+            if(match(arr,arr2)){
+                return true;
+            }
         }
         return false;
     }
-
-    static boolean match(String sortedS1, String windowStr) {
-        char[] arr1 = sortedS1.toCharArray();
-        char[] arr2 = windowStr.toCharArray();
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1, arr2);
+    static boolean match(char arr[],char arr2[]){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
