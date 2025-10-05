a334529fd14e570f02ed6d3c74f005d83ccdba51
remanshu12 <remanshugoyal1@gmail.com>
2025-10-06 03:52:49 +0530

    Time: 1 ms (19.18%) | Memory: 41.5 MB (80.41%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,41 @@
 class Solution {
-    public int strStr(String college, String clas) {
-        if(clas.length()> college.length()){
-            return -1;
-        }
-        for(int i=0;i<=college.length()-clas.length();i++){
-            for(int j=0;j<clas.length();j++){
-                if(college.charAt(i+j)!=clas.charAt(j)){
-                    break;
-                }
-                if(j==clas.length()-1){
-                    return i;
+    public int strStr(String s1, String s2) {
+        int lps[]=new int[s2.length()];
+        lps[0]=0;
+        int i=1;
+        int length=0;
+        while(i<s2.length()){
+            if(s2.charAt(length)==s2.charAt(i)){
+                length++;
+                lps[i]=length;
+                i++;
+            }else{
+                if(length!=0){
+                    length=lps[length-1];
+                }else{
+                    lps[i]=0;
+                    i++;
                 }
             }
         }
+        i=0;
+        int j=0;
+        while(i<s1.length()){
+            if(s1.charAt(i)==s2.charAt(j)){
+               i++;
+                j++;
+            }else{
+                if(j!=0)
+                j=lps[j-1];
+                else{
+                    i++;
+                }
+            }
+            if(j==s2.length()){
+                return i-s2.length();
+                
+            }
+        }
         return -1;
-       
-
     }
 }
