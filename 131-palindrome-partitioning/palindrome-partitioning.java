d84e61aa68a7720e31acbf19c8bb424877b5eb68
remanshu12 <remanshugoyal1@gmail.com>
2025-10-06 23:41:37 +0530

    Time: 8 ms (49.53%) | Memory: 56.7 MB (81.98%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -6,18 +6,18 @@ class Solution {
         return res;
     }
     static void helper(List<List<String>> res,List<String> ans,String s){
-        if(s==""){
-            res.add(new ArrayList<>(ans));
-            return;
+      if(s==""){
+        res.add(new ArrayList<>(ans));
+        return;
+      }
+      for(int i=0;i<s.length();i++){
+        String last=s.substring(0,i+1);
+        if(palindrome(last)){
+            ans.add(last);
+            helper(res,ans,s.substring(i+1));
+            ans.remove(ans.size()-1);
         }
-        for(int i=0;i<s.length();i++){
-            String last=s.substring(0,i+1);
-            if(palindrome(last)){
-                ans.add(last);
-                helper(res,ans,s.substring(i+1));
-                ans.remove(ans.size()-1);
-            }
-        }
+      }
     }
     static boolean palindrome(String s){
         int i=0;
