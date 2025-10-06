f37645033e55f17c26bcae8711c02024a697e210
remanshu12 <remanshugoyal1@gmail.com>
2025-10-06 10:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (80.41%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,19 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int haylength=haystack.length();
+        int needlelen=needle.length();
+        if(haylength<needlelen){
+            return -1;
+        }
+        for(int i=0;i<=haylength-needlelen;i++){
+            int j=0;
+            while(haystack.charAt(i+j)==needle.charAt(j)){
+                j++;
+                if(j==needlelen){
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
