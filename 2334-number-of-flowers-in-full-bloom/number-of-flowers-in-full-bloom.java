4b6eab2c87a0711ad7b0d644ba1f7a2a5265860d
remanshu12 <remanshugoyal1@gmail.com>
2025-10-14 04:55:55 +0530

    Time: 33 ms (97.25%) | Memory: 77.9 MB (48.49%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -1,4 +1,46 @@
 class Solution {
+    static int Upperbound(int arr[],int target){
+        if(target<arr[0]){
+            return 0;
+        }
+        if(target>=arr[arr.length-1]){
+            return arr.length;
+        }
+        int low=0;
+        int high=arr.length-1;
+        int ans=arr.length;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    static int Lowerbound(int arr[],int target){
+        if(target<arr[0]){
+            return 0;
+        }
+        if(target>arr[arr.length-1]){
+            return arr.length;
+        }
+        int low=0;
+        int high=arr.length-1;
+        int ans=arr.length;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
     
     public static int[] fullBloomFlowers(int[][] flowers, int[] people) {
         int n = flowers.length;
@@ -29,39 +71,7 @@ class Solution {
     }
 
 
-    public static int Upperbound(int[] arr, int target) { // first index > target
-        if (target < arr[0]) return 0;
-        if (target >= arr[arr.length - 1]) return arr.length;
-
-        int l = 0, r = arr.length - 1, ans = arr.length;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] > target) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return ans;
-    }
-
-    public static int Lowerbound(int[] arr, int target) { // first index >= target
-        if (target < arr[0]) return 0;
-        if (target > arr[arr.length - 1]) return arr.length;
-
-        int l = 0, r = arr.length - 1, ans = arr.length;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] >= target) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return ans;
-    }
+   
 }
 
 // blooming at time p=  flowers_started_on_or_before_p − flowers_ended_before_p
