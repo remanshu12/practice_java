04b656ee55e1bd0ae503f581d4b33d9d32e74208
remanshu12 <remanshugoyal1@gmail.com>
2025-11-13 07:42:07 -0800

    Time: 20 ms (74.90%) | Memory: 53.9 MB (12.03%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -10,17 +10,22 @@ class Solution {
 
     }
     int helper(String s1,String s2,int m,int n,int dp[][]){
-        if(n==0 || m==0){
-           return 0;
-        }
-    if(dp[m][n]!=-1){
-        return dp[m][n];
+       for(int i=0;i<m+1;i++){
+        for(int j=0;j<n+1;j++){
+            if(i==0 || j==0){
+                dp[i][j]=0;
             }
-        if(s1.charAt(m-1)==s2.charAt(n-1)){
-          return dp[m][n]=1+helper(s1,s2,m-1,n-1,dp);
         }
-        else{
-           return dp[m][n]=Math.max(helper(s1,s2,m-1,n,dp),helper(s1,s2,m,n-1,dp));
+       }
+       for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else{
+                dp[i][j]=Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
         }
+       }
+       return dp[m][n];
     }
 }
