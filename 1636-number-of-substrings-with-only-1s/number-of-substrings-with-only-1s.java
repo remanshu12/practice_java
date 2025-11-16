7493519de9574b6d400b44d267d5c9d4f790c7f3
remanshu12 <remanshugoyal1@gmail.com>
2025-11-15 19:48:59 -0800

    Time: 5 ms (57.07%) | Memory: 46.5 MB (6.02%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -6,13 +6,11 @@ class Solution {
             if(s.charAt(i)=='1'){
                 one++;
             }else {
-                total+=((one*(one+1))/2)%(long)1000000007;
-                
+                total+=((one*(one+1))/2)%(long)1000000007;   
                 one=0; 
-            }
-            
-        } total+=((one*(one+1))/2)%(long)1000000007;
-       
+            }    
+        } 
+        total+=((one*(one+1))/2)%(long)1000000007;   
         return (int)total;
     }
 }
