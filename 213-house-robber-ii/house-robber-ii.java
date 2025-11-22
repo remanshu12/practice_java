4ba3ad64bff43d7469db932e781ea0f9697e2d29
remanshu12 <remanshugoyal1@gmail.com>
2025-11-22 03:07:26 -0800

    Time: 0 ms (100.00%) | Memory: 42.9 MB (11.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,22 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int rob(int[] nums) {
-        int n= nums.length;
+        int n = nums.length;
         if (n == 1) return nums[0];
-        int [] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        int [] dp1 = new int[nums.length];
-        Arrays.fill(dp1,-1);
-        return Math.max (robber(nums,0,n-1,dp),robber(nums,1,n,dp1));
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        // Case 1: rob from 0 to n-2
+        // Case 2: rob from 1 to n-1
+        return Math.max(helper(nums, 0, n - 1), helper(nums, 1, n));
     }
-    public int robber ( int [] arr , int i ,int j, int dp[]) {
-        if(i>=j){
-            return 0;
+
+    static int helper(int[] nums, int start, int end) {
+        int[] dp = new int[end]; // only for this range
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+
+        for (int i = start + 2; i < end; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        if(dp[i]!=-1){ // because 0 can be the answer
-            return dp[i];
-        }
-        int rob =arr[i]+robber(arr,i+2,j,dp);
-        int dont_rob = robber(arr,i+1,j,dp);
-        return dp[i]= Math.max(rob , dont_rob);
+        
+        return dp[end - 1];
     }
 }
