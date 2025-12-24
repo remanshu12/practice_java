f70e7d8709c14e540cb9767de520cf848cbd520f
remanshu12 <remanshugoyal1@gmail.com>
2025-12-24 14:22:11 +0530

    Time: 10 ms (88.32%) | Memory: 47.8 MB (17.48%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int countCollisions(String D) {
-        final int n=D.length();
-        if (n==1) return 0;
-        int l=0, r=n-1;
-        while (l<r && D.charAt(l)=='L') l++;
-        while (l<r && D.charAt(r)=='R') r--;
-        if (l>=r) return 0;
-        int col=0;
-        for( ; l<=r; l++){
-           col+=(D.charAt(l)!='S'?1:0);
+    public int countCollisions(String s) {
+        int i=0;
+        int j=s.length()-1;
+        int n=s.length();
+        while(i<n && (s.charAt(i)=='L')){
+            i++;
         }
-        return col;  
+        while(j>=0 && (s.charAt(j)=='R')){
+            j--;
+        }
+        int count=0;
+        for(int k=i;k<=j;k++){
+            if(s.charAt(k)!='S'){
+                count++;
+            }
+        }
+        return count;
     }
 }
