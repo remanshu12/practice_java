b4166bdbd9d2c1da6fb0903cb21e24010468b19d
remanshu12 <remanshugoyal1@gmail.com>
2025-12-24 15:44:38 +0530

    Time: 8 ms (97.40%) | Memory: 51.7 MB (21.43%) - LeetSync

--- 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
+++ 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
@@ -1,13 +1,35 @@
 class Solution {
-       public String smallestString(String s) {
-        int i=0,n=s.length();
-        char[]A=s.toCharArray();
-        while (i<n&&A[i]=='a')
+    public String smallestString(String s) {
+        if (s.isEmpty()) return ""; // Handle empty string
+        
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        
+        // Check if all characters are 'a'
+        boolean allAs = true;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != 'a') {
+                allAs = false;
+                break;
+            }
+        }
+        
+        // If all 'a's, replace last character with 'z'
+        if (allAs) {
+            sb.setCharAt(n - 1, 'z');
+            return sb.toString();
+        }
+        
+        // Find first non-'a' substring and replace with predecessors
+        int i = 0;
+        while (i < n && s.charAt(i) == 'a') {
             i++;
-        if (i==n)
-            A[n-1]='z';
-        while (i<n &&A[i]!='a')
-            --A[i++];
-        return String.valueOf(A);
+        }
+        while (i < n && s.charAt(i) != 'a') {
+            sb.setCharAt(i, (char)(s.charAt(i) - 1));
+            i++;
+        }
+        
+        return sb.toString();
     }
 }
