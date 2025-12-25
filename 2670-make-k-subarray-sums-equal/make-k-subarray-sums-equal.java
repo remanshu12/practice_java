7b43195dd0351102227bfaf930a0ce798b9f454b
remanshu12 <remanshugoyal1@gmail.com>
2025-12-25 21:24:23 +0530

    Time: 114 ms (20.69%) | Memory: 100.9 MB (77.59%) - LeetSync

---
+++ 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long makeSubKSumEqual(int[] arr, int k) {
+         long res=0;
+        for (int i=0;i<k;++i) { 
+            List<Integer> cycle = new ArrayList<>();
+            for(int j=i;arr[j]!=0;j=(j+k)%arr.length) {
+                cycle.add(arr[j]);
+                arr[j] = 0;
+            }
+            Collections.sort(cycle);
+            for (int n:cycle)
+                res+=Math.abs(n-cycle.get(cycle.size()/2));
+        }
+        return res;
+    }
+     int gcd(int a, int b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
+    }
+
+}
