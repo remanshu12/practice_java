ca3627673695b70ebf64a353e0e920242867c470
remanshu12 <remanshugoyal1@gmail.com>
2025-12-26 12:57:38 +0530

    Time: 704 ms (5.11%) | Memory: 83.1 MB (90.64%) - LeetSync

---
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int partitionDisjoint(int[] nums) {
+        int min[]=new int[nums.length];
+        int n=nums.length;
+        min[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            min[i]=Math.min(min[i+1],nums[i]);
+        }
+        //System.out.println(Arrays.toString(min));
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+            System.out.println(max+" "+min[i+1]);
+            if(max<=min[i+1]){
+                return i+1;
+            }
+        }
+        return 1;
+    }
+}
