88280b8933c144788b51373ea4735feaee7ecd0f
remanshu12 <remanshugoyal1@gmail.com>
2025-12-30 13:23:21 +0530

    Time: 19 ms (30.98%) | Memory: 47.1 MB (94.56%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,51 +1,49 @@
- class tuple{
-            int first;
-            int second;
-            int third;
-            tuple(int f,int s,int t){
-                first=f;
-                second=s;
-                third=t;
-            }
-            
-        }
 class Solution {
+    class polak{
+        int first;
+        int second;
+        int third;
+        polak(int f,int s,int t){
+            first=f;
+            second=s;
+            third=t;
+        }
+    }
     public int shortestPathBinaryMatrix(int[][] grid) {
-       Queue<tuple> pq=new LinkedList<>();
-      
         int n=grid.length;
         int m=grid[0].length;
-           
-
-        // If start or end blocked
-        if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
-
-        // Single cell case
-        if (n == 1 && m == 1) return 1;
-        int dist[][]=new int[n][m];
+        int[][] dist=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 dist[i][j]=(int)1e9;
             }
-        } 
+        }
+        if(grid[0][0]==1 || grid[n-1][m-1]==1){
+            return -1;
+        }
+        if(n==1 && m==1){
+            return 1;
+        }
+        
         dist[0][0]=1;
-        pq.add(new tuple(1,0,0));
-        int dr[]={-1,-1,0,+1,+1,+1,0,-1};
-        int dc[]={0,+1,+1,+1,0,-1,-1,-1};
-        while(!pq.isEmpty()){
-            int dis=pq.peek().first;
-            int r=pq.peek().second;
-            int c=pq.peek().third;
-            pq.poll();
+        Queue<polak> q=new LinkedList<>();
+        q.add(new polak(1,0,0));
+        while(!q.isEmpty()){
+            int dis=q.peek().first;
+            int row=q.peek().second;
+            int col=q.peek().third;
+            q.poll();
+            int drow[]={-1,-1,-1,0,0,+1,1,1};
+            int dcol[]={-1,0,1,-1,+1,-1,0,1};
             for(int i=0;i<8;i++){
-                int newrow=r+dr[i];
-                int newcol=c+dc[i];
-                if(newrow>=0 && newrow<n && newcol>=0 && newcol<m && grid[newrow][newcol]==0 && dis+1<dist[newrow][newcol]){
+                int newrow=row+drow[i];
+                int newcol=col+dcol[i];
+                if(newrow>=0 && newcol>=0 && newrow<n && newcol<m && grid[newrow][newcol]==0 && dis+1<dist[newrow][newcol]){
                     dist[newrow][newcol]=dis+1;
                     if(newrow==n-1 && newcol==m-1){
                         return dis+1;
                     }
-                    pq.add(new tuple(dis+1,newrow,newcol));
+                    q.add(new polak(dis+1,newrow,newcol));
                 }
             }
         }
