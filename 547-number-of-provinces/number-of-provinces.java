e7d1cf387012693826e2fe0f62badbe8757e3081
remanshu12 <remanshugoyal1@gmail.com>
2025-12-30 21:14:32 +0530

    Time: 2 ms (48.13%) | Memory: 50 MB (6.27%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,38 +1,35 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int n = isConnected.length;
-
-        // build adjacency list
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<Integer>());
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int n=isConnected.length;
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
                     adj.get(i).add(j);
-                    adj.get(j).add(i);
+
                 }
             }
         }
-
-        int visited[] = new int[n];
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
+        int visited[]=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
                 count++;
-                dfs(visited, adj, i);
+                dfs(visited,adj,i);
             }
         }
+        
+
         return count;
     }
-
-    static void dfs(int visited[], ArrayList<ArrayList<Integer>> adj, int curr) {
-        visited[curr] = 1;
-        for (int neighbor : adj.get(curr)) {
-            if (visited[neighbor] == 0) {
-                dfs(visited, adj, neighbor);
+    static void dfs(int[] visited,ArrayList<ArrayList<Integer>> adj,int curr){
+        visited[curr]=1;
+        for(int neigh:adj.get(curr)){
+            if(visited[neigh]==0){
+                dfs(visited,adj,neigh);
             }
         }
     }
