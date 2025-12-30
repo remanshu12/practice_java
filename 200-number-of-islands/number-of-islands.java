be402fa1f508445d3d5aab092a3378d54c590413
remanshu12 <remanshugoyal1@gmail.com>
2025-12-30 12:19:25 +0530

    Time: 6 ms (24.48%) | Memory: 51.4 MB (83.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,7 +1,6 @@
 class Solution {
-     static class pair{
-        int first;
-        int second;
+    static class pair{
+        int first,second;
         pair(int first,int second){
             this.first=first;
             this.second=second;
@@ -16,30 +15,35 @@ class Solution {
             for(int j=0;j<m;j++){
                 if(visited[i][j]==0 && grid[i][j]=='1'){
                     count++;
-                    bfs(visited,i,j,n,m,grid);
+                    bfs(i,j,grid,visited,n,m);
                 }
             }
         }
         return count;
     }
-    static void bfs(int[][] visited,int row,int col,int n,int m,char[][] grid ){
-        int[] delrow={-1,0,1,0};
-        int[] delcol={0,1,0,-1};
-        visited[row][col]=1;
-        Queue<pair> q=new LinkedList<>();
-        q.add(new pair(row,col));
-        while(!q.isEmpty()){
-             row=q.peek().first;
-             col=q.peek().second;
+        static void bfs(int ro,int co,char[][] grid,int[][] visited,int n,int m){
+            visited[ro][co]=1;
+            Queue<pair> q=new LinkedList<>();
+           q.add(new pair(ro,co));
+           int drow[]={1,0,-1,0};
+            int dcol[]={0,1,0,-1};
+            while(!q.isEmpty()){
+                 
+            int row=q.peek().first;
+            int col=q.peek().second;
             q.poll();
-        for(int i=0;i<4;i++){
-            int newrow=row+delrow[i];
-            int newcol=col+delcol[i];
-            if(newrow>=0 && newrow<n && newcol>=0 && newcol<m && visited[newrow][newcol]==0 && grid[newrow][newcol]=='1'){
-                visited[newrow][newcol]=1;
-                q.add(new pair(newrow,newcol));
+                
+                for(int i=0;i<4;i++){
+                   
+                        int newrow=row+drow[i];
+                        int newcol=col+dcol[i];
+                        if(newrow>=0 && newcol >=0 && newrow<n && newcol<m && visited[newrow][newcol]==0 && grid[newrow][newcol]=='1'){
+                            visited[newrow][newcol]=1;
+                            q.add(new pair(newrow,newcol));
+                        }
+                    
+                }
             }
         }
-     }
-    }
+    
 }
