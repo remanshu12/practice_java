0e97895fa4e8be2180924265496081d5eca35ff1
remanshu12 <remanshugoyal1@gmail.com>
2026-01-02 19:57:42 +0530

    Time: 765 ms (5.41%) | Memory: 130.9 MB (15.68%) - LeetSync

---
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int edgeScore(int[] arr) {
+        HashMap<Long,Long> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put((long)arr[i],map.getOrDefault((long)arr[i],(long)0)+(long)i);
+        }
+        long value=0;
+        long key=0;
+        for(long i:map.keySet()){
+            System.out.println(i+" "+map.get(i));
+           if(value<map.get(i)){
+            value=map.get(i);
+            key=i;
+           }else if(value==map.get(i)){
+            key=Math.min(key,i);
+           }
+        }
+        return (int)key;
+    }
+}
