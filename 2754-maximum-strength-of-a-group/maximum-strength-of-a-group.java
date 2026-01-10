0568e730dc7b9c4b5b344ab5ce7daeb3c1386f27
remanshu12 <remanshugoyal1@gmail.com>
2026-01-10 18:05:51 +0530

    Time: 12 ms (35.23%) | Memory: 46.2 MB (5.11%) - LeetSync

---
+++ 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long maxStrength(int[] nums) {
+        Arrays.sort(nums);
+        long sum=1;
+        int count=0;
+        int zero=0;
+        int one=0;
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i:nums){
+            if(i>0){
+                sum*=i;
+                one++;
+            }else if(i<0){
+                count++;
+            }else{
+                zero++;
+            }
+        }
+        if(zero==nums.length){
+            return 0;
+        }
+        if(one==0 && count==1 && zero>=1){
+            return 0;
+        }
+        System.out.println(Arrays.toString(nums));
+        count=count/2;
+        count=count*2;
+        for(int i=0;i<count;i++){
+            sum*=nums[i];
+        }
+        return sum;
+    }
+}
