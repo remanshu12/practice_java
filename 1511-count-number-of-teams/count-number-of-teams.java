42801b9863d99dc21e71dcd934cf9b71e78646a3
remanshu12 <remanshugoyal1@gmail.com>
2026-01-20 15:35:51 +0530

    Time: 16 ms (75.17%) | Memory: 44.4 MB (40.92%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -1,15 +1,37 @@
 class Solution {
     public int numTeams(int[] rating) {
-        int count=0;
-        for(int i=0;i<rating.length-2;i++){
-            for(int j=i+1;j<rating.length-1;j++){
-                for(int k=j+1;k<rating.length;k++){
-                    if((rating[i]<rating[j] && rating[j]<rating[k])||(rating[i]>rating[j] && rating[j]>rating[k])){
-                        count++;
-                    }
-                }
+        int leftsmall=0;
+        int rightlarge=0;
+        int leftlarge=0;
+        int rightsmall=0;
+        int res=0;
+        for(int i=1;i<rating.length-1;i++){
+             leftsmall=0;
+              rightlarge=0;
+            leftlarge=0;
+            rightsmall=0;
+            int left=i-1;
+            int right=i+1;
+            while(left>=0){
+            if(rating[left]<rating[i]){
+                leftsmall++;
+            }else if(rating[left]>rating[i]){
+                leftlarge++;
             }
+            left--; 
+            }
+            while(right<rating.length){
+                if(rating[right]>rating[i]){
+                    rightlarge++;
+                }else if(rating[right]<rating[i]){
+                    rightsmall++;
+                }
+                right++;
+            }
+            res+=rightsmall*leftlarge;
+            res+=leftsmall*rightlarge;
         }
-        return count;
+
+        return res;
     }
 }
