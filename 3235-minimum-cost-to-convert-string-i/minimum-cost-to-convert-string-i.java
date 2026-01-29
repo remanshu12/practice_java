15d8fac4288d831bdc7ab55c1ea7084d8af7e1f7
remanshu12 <remanshugoyal1@gmail.com>
2026-01-29 18:45:02 +0530

    Time: 24 ms (56.27%) | Memory: 48.1 MB (30.82%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        final long inf=(long)1e18;
+        long[][] dist=new long[26][26];
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                dist[i][j]=(i==j)?0:inf;
+            }
+        }
+        for(int i=0;i<original.length;i++){
+            int u=original[i]-'a';
+            int v=changed[i]-'a';
+            dist[u][v]=Math.min(dist[u][v],cost[i]);
+        }
+        for(int k=0;k<26;k++){
+            for(int i=0;i<26;i++){
+                for(int j=0;j<26;j++){
+                    if(dist[i][k]+dist[k][j]<dist[i][j]){
+                        dist[i][j]=dist[i][k]+dist[k][j];
+                    }
+                }
+            }
+        }
+        long ans=0;
+        for(int i=0;i<source.length();i++){
+            int s=source.charAt(i)-'a';
+            int t=target.charAt(i)-'a';
+            if(dist[s][t]==inf) return -1;
+            ans+=dist[s][t];
+        }
+        return ans;
+    }
+}
