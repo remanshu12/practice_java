27ee5e1c3ef2aacc498cb626db63131ce5fb0042
remanshu12 <remanshugoyal1@gmail.com>
2026-01-29 11:06:26 +0530

    Time: 6 ms (100.00%) | Memory: 102.8 MB (21.69%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -10,9 +10,9 @@ class Solution {
         }
         
       } 
-     // System.out.println(Arrays.toString(nums));
+    
       int n=nums.length;
-        if(nums[n-1]==0 || nums[n-2]==0 || nums[n-3]==0 ){
+        if(nums[n-1]==0 || nums[n-2]==0 ){
             return -1;
         }
         return count;
