c51703167cb70a6b83b1c2d9ebfcdaabe72c0c05
remanshu12 <remanshugoyal1@gmail.com>
2026-02-03 12:39:01 +0530

    Time: 10 ms (3.36%) | Memory: 45.1 MB (10.08%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int diff[]=new int[nums.length-1];
+        for(int i=0;i<nums.length-1;i++){
+            diff[i]=nums[i+1]-nums[i];
+            if(diff[i]==0){
+                return false;
+            }
+        }
+        System.out.println(Arrays.toString(diff));
+        if(diff[0]<0) return false;
+            if(diff[diff.length-1]<0) return false;
+            int neg=0;
+            int pos=0;
+            boolean flag=false;
+        for(int i=0;i<diff.length;i++){
+               if(diff[i]>0 && flag==false){
+                pos++;
+                flag=true;
+               }else if(diff[i]<0 && flag==true){
+                neg++;
+                flag=false;
+               }
+
+        }
+         System.out.println(neg+" " + pos);
+        return neg==1 && pos==2;
+    }
+}
