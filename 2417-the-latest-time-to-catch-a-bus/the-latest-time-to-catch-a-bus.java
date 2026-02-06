e7aaf58675e2f7d79443528c491bc937700e8db2
remanshu12 <remanshugoyal1@gmail.com>
2026-02-06 14:53:50 +0530

    Time: 31 ms (72.22%) | Memory: 97.2 MB (77.08%) - LeetSync

--- 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.java
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.java
@@ -2,31 +2,30 @@ class Solution {
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
         Arrays.sort(buses);
         Arrays.sort(passengers);
-        int i=0;
-        int j=0;
         int n=buses.length;
         int m=passengers.length;
-            int curr=0;
-
-        for( i=0;i<n;i++){
+        int j=0;
+        int curr=0;
+        for(int i=0;i<n;i++){
             curr=0;
-            while(j<m && curr<capacity && passengers[j]<=buses[i]){
+            while(j<m &&curr<capacity && passengers[j]<=buses[i]){
                 curr++;
                 j++;
             }
+
         }
+        j-=1;
         int time=0;
-        if(curr==capacity){
-            time=passengers[j-1];
+        if(curr<capacity){
+            time= buses[n-1];
         }else{
-            time=buses[n-1];
+            time=passengers[j];
         }
-        j--;
-        while(j>=0 && passengers[j]==time){
+        System.out.println(j);
+        while(j>=0 && time==passengers[j]){
             j--;
             time--;
         }
         return time;
-
     }
 }
