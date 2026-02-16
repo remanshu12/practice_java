130cf546ec9b3b87af0cc80fab32d57193debb62
remanshu12 <remanshugoyal1@gmail.com>
2026-02-17 00:30:05 +0530

    Time: 47 ms (17.45%) | Memory: 24.5 MB (63.08%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -6,13 +6,15 @@ public:
      if(grid[0][0] ==1 || grid[n-1][m-1]==1){
         return -1;
      }
-     queue<tuple<int,int,int>> q;
+     priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> q;
      int dx[]={1,0,-1,0,1,-1,1,-1};
      int dy[]={0,1,0,-1,1,-1,-1,1};
      q.push({0,0,1});
-     grid[0][0]=1;
+    
+     vector<vector<int>> dist(n,vector<int>(m,INT_MAX));
+     dist[0][0]=1;
      while(!q.empty()){
-        auto[x,y,cnt]=q.front();
+        auto[x,y,cnt]=q.top();
         
         q.pop();
         if(x==n-1 && y==m-1){
@@ -22,8 +24,12 @@ public:
             int newrow=x+dx[i];
             int newcol=y+dy[i];
             if(newrow>=0 && newcol >=0 && newrow<n && newcol <m && grid[newrow][newcol]==0){
+                if(cnt+1<dist[newrow][newcol]){
+                    dist[newrow][newcol]=cnt+1;
                     q.push({newrow,newcol,cnt+1});
-                    grid[newrow][newcol]=1;
+                }
+                    
+                    
             }
         }
      }
