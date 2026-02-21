b63707d150c30047e16667dd65b60a2765827f2e
remanshu12 <remanshugoyal1@gmail.com>
2026-02-21 14:18:00 +0530

    Time: 3 ms (30.05%) | Memory: 16.2 MB (60.33%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -3,34 +3,21 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<int> prev(m,0);
+        vector<vector<int>> dp(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
-            vector<int> curr(m,0);
             for(int j=0;j<m;j++){
-                if(i==0 && j==0 ){
-                    curr[j]=grid[i][j];
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
                     continue;
                 }
-                int up=0;
-                int left=0;
-                if(i>0) up=prev[j];
-                else{
-                    up=curr[j-1];
-                }
-                if(j>0) left=curr[j-1];
-                else{
-                    left=prev[j];
-                }
-                curr[j]=grid[i][j]+min(up,left);
-
-            }prev=curr;
+                int left=INT_MAX;
+                int up=INT_MAX;
+                int val=grid[i][j];
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                dp[i][j]=val+min(left,up);
+            }
         }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << '\n';
-        // }
-        return prev[m-1];
+        return dp[n-1][m-1];
     }
 };
