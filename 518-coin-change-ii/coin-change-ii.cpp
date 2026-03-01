56d2730800c68d6319711c2146ae468ed9247a93
remanshu12 <remanshugoyal1@gmail.com>
2026-03-01 11:49:21 +0530

    Time: 35 ms (40.00%) | Memory: 36.6 MB (54.77%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int change(int sum, vector<int>& coins) {
+          int n=coins.size();
+        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(sum+1));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0){
+                    dp[i][j]=0;
+                }
+                if(j==0){
+                    dp[i][j]=1;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(j>=coins[i-1]){
+                    dp[i][j]=(dp[i][j-coins[i-1]]+dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+};
