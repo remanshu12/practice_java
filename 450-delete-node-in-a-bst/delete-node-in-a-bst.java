ccc03e9a4aa491cc69f29540dff9b43428506e31
remanshu12 <remanshugoyal1@gmail.com>
2026-03-13 14:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (14.59%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -15,47 +15,39 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null){
-            return null;
-        }
-        if(root.val==key){
-            return helper(root);
-        }
+        if(root==null) return null;
+        if(root.val==key) return helper(root);
         TreeNode dummy=root;
         while(root!=null){
             if(root.val>key){
                 if(root.left!=null && root.left.val==key){
                     root.left=helper(root.left);
-                    break;
                 }else{
                     root=root.left;
                 }
             }else{
                 if(root.right!=null && root.right.val==key){
                     root.right=helper(root.right);
-                    break;
                 }else{
                     root=root.right;
                 }
             }
         }
-        return dummy;
+        return dummy;           
+  
     }
     static TreeNode helper(TreeNode root){
-        if(root.left==null){
-            return root.right;
-        }else if(root.right==null){
-            return root.left;
-        }
+        if(root.left==null) return root.right;
+        else if(root.right==null) return root.left;
         TreeNode rightchild=root.right;
-        TreeNode leftlastchild=findlastchild(root.left);
-        leftlastchild.right=rightchild;
-        return root.left;
+       TreeNode lastleftrightchild=findlast(root.left);
+       lastleftrightchild.right=rightchild;
+       return root.left;
     }
-    static TreeNode findlastchild(TreeNode root){
+    static TreeNode findlast(TreeNode root){
         if(root.right==null){
             return root;
         }
-        return findlastchild(root.right);
+       return findlast(root.right);
     }
 }
