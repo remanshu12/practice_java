5810ed92330642c82504212a112479be4c24ea46
remanshu12 <remanshugoyal1@gmail.com>
2026-03-14 07:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (66.71%) - LeetSync

---
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int moves=0;
+
+    public int distributeCoins(TreeNode root) {
+        moves=0;
+        helper(root);    
+        return moves;
+    }
+    static int helper(TreeNode root){
+        if(root==null) return 0;
+        int left=helper(root.left);
+        int right=helper(root.right);
+        moves+=Math.abs(left)+Math.abs(right);
+        return root.val+left+right-1;
+    }
+}
