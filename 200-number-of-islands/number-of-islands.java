3345aefeca90d909f7f5e547716c4015cf87e42f
remanshu12 <remanshugoyal1@gmail.com>
2026-03-16 11:20:05 +0530

    Time: 3 ms (86.77%) | Memory: 52.5 MB (43.26%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,49 +1,25 @@
 class Solution {
-    static class pair{
-        int first,second;
-        pair(int first,int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int[][] visited=new int[n][m];
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visited[i][j]==0 && grid[i][j]=='1'){
+                if(grid[i][j]=='1'){
                     count++;
-                    bfs(i,j,grid,visited,n,m);
-                }
+                dfs(i,j,grid,n,m);}
             }
         }
         return count;
     }
-        static void bfs(int ro,int co,char[][] grid,int[][] visited,int n,int m){
-            visited[ro][co]=1;
-            Queue<pair> q=new LinkedList<>();
-           q.add(new pair(ro,co));
-           int drow[]={1,0,-1,0};
-            int dcol[]={0,1,0,-1};
-            while(!q.isEmpty()){
-                 
-            ro=q.peek().first;
-            co=q.peek().second;
-            q.poll();
-                
-                for(int i=0;i<4;i++){
-                   
-                        int newrow=ro+drow[i];
-                        int newcol=co+dcol[i];
-                        if(newrow>=0 && newcol >=0 && newrow<n && newcol<m && visited[newrow][newcol]==0 && grid[newrow][newcol]=='1'){
-                            visited[newrow][newcol]=1;
-                            q.add(new pair(newrow,newcol));
-                        }
-                    
-                }
-            }
+    static void dfs(int row,int col,char[][] grid,int n ,int m){
+        if(row<0 || col <0 ||row>=n  || col>=m || grid[row][col]=='0'){
+            return;
         }
-    
+        grid[row][col]='0';
+       dfs(row+1,col,grid,n,m);
+       dfs(row-1,col,grid,n,m);
+       dfs(row,col+1,grid,n,m);
+       dfs(row,col-1,grid,n,m);
+    }
 }
