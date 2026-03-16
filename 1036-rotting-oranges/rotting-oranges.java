e74339ed57842d025d8bcaf578bd10718681ddd3
remanshu12 <remanshugoyal1@gmail.com>
2026-03-16 11:07:41 +0530

    Time: 3 ms (11.71%) | Memory: 43.8 MB (96.93%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,43 +1,38 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid==null || grid.length==0){
-            return -1;
-        } 
+        int total=0;
+        Queue<int[]>q=new LinkedList<>();
         int n=grid.length;
         int m=grid[0].length;
-        int fresh=0;
-        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
+                if(grid[i][j]==1){
+                    total++;
+                }else if(grid[i][j]==2){
                     q.add(new int[]{i,j,0});
-                }else if(grid[i][j]==1){
-                    fresh++;
                 }
             }
         }
-        if(fresh==0){
-            return 0;
-        }
-        int time=0;
-        int delrow[]={-1,0,+1,0};
-        int delcol[]={0,+1,0,-1};
+        int max=0;
+      if(total==0) return 0;
+         int rowArr[]={-1,1,0,0};
+            int colArr[]={0,0,1,-1};
         while(!q.isEmpty()){
             int row=q.peek()[0];
             int col=q.peek()[1];
-            int val=q.peek()[2];
+            int time=q.peek()[2];
+           max=Math.max(max,time);
             q.poll();
-            time=Math.max(time,val);
             for(int i=0;i<4;i++){
-                int newrow=row+delrow[i];
-                int newcol=col+delcol[i];
+                int newrow=row+rowArr[i];
+                int newcol=col+colArr[i];
                 if(newrow>=0 && newrow<n && newcol>=0 && newcol<m && grid[newrow][newcol]==1){
                     grid[newrow][newcol]=2;
-                    fresh--;
+                    total--;
                     q.add(new int[]{newrow,newcol,time+1});
                 }
             }
         }
-        return fresh==0 ? time:-1;
+        return total==0?max:-1;
     }
 }
