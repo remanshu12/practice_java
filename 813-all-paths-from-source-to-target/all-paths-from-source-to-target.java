219871c8f8353d39e90e592009e41c99b2c1818c
remanshu12 <remanshugoyal1@gmail.com>
2026-03-17 01:19:44 +0530

    Time: 2 ms (91.37%) | Memory: 47.8 MB (83.92%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        temp.add(0);
-        plku(list,graph,0,graph.length-1,temp);
-        return list;
-    }
-    static void plku( List<List<Integer>> list,int[][] graph, int s , int e,List<Integer> temp){
-        if(s==e){
-            list.add(new ArrayList<>(temp));
-            return ;
-        }
-       
+         List<List<Integer>> list=new ArrayList<>();
+         int n=graph.length;
         
-        for(int ed:graph[s]){
-           
-                temp.add(ed);
-                plku(list,graph,ed,e,temp);
-                temp.remove(temp.size()-1);
+            ArrayList<Integer> ans=new ArrayList<>();
+            ans.add(0);
+          //  System.out.println(ans.size());
+            dfs(0,list,n,ans,graph);
+         
+         return list;
+    }
+    static void dfs(int node,List<List<Integer>> list,int n,ArrayList<Integer> ans,int[][] graph){
+        if(node==n-1){ 
+            list.add(new ArrayList<>(ans));
+            return;
+        }
+        for(int nd:graph[node]){
+            ans.add(nd);
             
+            dfs(nd,list,n,ans,graph);
+             ans.remove(ans.size()-1);
         }
        
     }
