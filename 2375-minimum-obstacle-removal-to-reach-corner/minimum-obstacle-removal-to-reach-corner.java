43d35cf6d461b84a7b268d244c596ac0ed61ff7d
remanshu12 <remanshugoyal1@gmail.com>
2026-03-17 17:06:39 +0530

    Time: 135 ms (52.77%) | Memory: 126.4 MB (86.62%) - LeetSync

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimumObstacles(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        boolean visited[][]=new boolean[n][m];
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+      //  int obs=0;
+        int rowArr[]={-1,1,0,0};
+        int colArr[]={0,0,-1,1};
+        pq.add(new int[]{grid[0][0],0,0});
+        int min=Integer.MAX_VALUE;
+        while(!pq.isEmpty()){
+            int obs=pq.peek()[0];
+            int row=pq.peek()[1];
+            int col=pq.peek()[2];
+            if(row==n-1 && col==m-1){
+                min=Math.min(min,obs);
+            }
+            pq.poll();
+            for(int i=0;i<4;i++){
+                int newrow=row+rowArr[i];
+                int newcol=col+colArr[i];
+                if(newrow>=0 && newcol>=0 && newrow<n && newcol<m && visited[newrow][newcol]!=true){
+                    visited[newrow][newcol]=true;
+                    pq.add(new int[]{obs+grid[newrow][newcol],newrow,newcol});
+                }
+            }
+        }
+        return min;
+        
+    }
+}
