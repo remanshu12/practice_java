a4b65c8f6315e4ac388580d9679c241915a847da
remanshu12 <remanshugoyal1@gmail.com>
2026-03-17 15:33:24 +0530

    Time: 10 ms (78.07%) | Memory: 46.2 MB (83.02%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,39 +1,34 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int m=grid.length, n=grid[0].length;
-        int[][] directions={{0,1}, {1,0}, {0,-1}, {-1,0}};
-        
-        int lo = grid[0][0], hi=0;
-        for (int[]row:grid)
-            for (int val:row)
-                hi = Math.max(hi, val);
-        
-        while (lo < hi) {
-            int mid = lo+(hi-lo)/2;
-            if (possible(grid, mid, m, n, directions)) hi = mid;
-            else lo = mid + 1;
-        }
-        return lo;
-    }
-    
-    private boolean possible(int[][] grid, int mid, int m, int n, int[][] directions) {
-        if (grid[0][0] > mid) return false;
-        boolean[][] seen = new boolean[m][n];
-        return dfs(grid, 0, 0, mid, seen, m, n, directions);
-    }
-    
-    private boolean dfs(int[][] grid, int r, int c, int mid, boolean[][] seen, int m, int n, int[][] directions) {
-        if (r == m-1 && c == n-1) return true;
-        seen[r][c] = true;
-        
-        for (int[] dir : directions) {
-            int nr = r + dir[0], nc = c + dir[1];
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !seen[nr][nc]) {
-                if (grid[nr][nc] <= mid) {
-                    if (dfs(grid, nr, nc, mid, seen, m, n, directions)) return true;
+        int n=grid.length;
+        int m=grid[0].length;
+        boolean visited[][]=new boolean[n][m];
+        visited[0][0]=true;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        pq.add(new int[]{grid[0][0],0,0});
+         int rowArr[]={-1,1,0,0};
+        int colArr[]={0,0,-1,1};
+int max=grid[0][0];
+        while(!pq.isEmpty()){
+            int row=pq.peek()[1];
+            int col=pq.peek()[2];
+            int dis=pq.peek()[0];
+            pq.poll();
+                if(row==n-1 && col==m-1){
+                return dis;
+            }
+            for(int i=0;i<4;i++){
+                int newrow=row+rowArr[i];
+                int newcol=col+colArr[i];
+                if(newrow>=0 && newcol>=0 && newrow<n && newcol<m && visited[newrow][newcol]!=true){
+                    visited[newrow][newcol]=true;
+                    max=Math.max(dis,grid[newrow][newcol]);
+                 
+                    pq.add(new int[]{max,newrow,newcol});
                 }
             }
+
         }
-        return false;
+        return -1;
     }
 }
