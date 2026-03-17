e750485f4f22b9750e6d948b31e0fa044c677beb
remanshu12 <remanshugoyal1@gmail.com>
2026-03-17 16:22:16 +0530

    Time: 14 ms (45.09%) | Memory: 51.9 MB (61.79%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        int mod=1_000_000_007;
-        List<List<int[]>> list=new ArrayList<>();
+        int mod =(int)1000000007;
+          List<List<int[]>> list=new ArrayList<>();
         for(int i=0;i<n;i++){
             list.add(new ArrayList<>());
         }
@@ -14,28 +14,27 @@ class Solution {
         }
         long dist[]=new long[n];
         Arrays.fill(dist,Long.MAX_VALUE);
+        long count[]=new long[n];
+        count[0]=1;
         dist[0]=0;
-        int ways[]=new int[n];
-        ways[0]=1;
         PriorityQueue<long[]> pq=new PriorityQueue<>((a,b)->Long.compare(a[0],b[0]));
         pq.add(new long[]{0,0});
         while(!pq.isEmpty()){
-            long d=pq.peek()[0];
-            int u=(int)pq.peek()[1];
+            long dis=pq.peek()[0];
+            int node=(int)pq.peek()[1];
             pq.poll();
-            if(d>dist[u]) continue;
-            for(int[] edge:list.get(u)){
-                int v=edge[0];
-                long w=edge[1];
-                if(dist[v]>dist[u]+w){
-                    dist[v]=dist[u]+w;
-                    ways[v]=ways[u];
-                    pq.add(new long[]{dist[v],v});
-                }else if(dist[u]+w==dist[v]){
-                   ways[v]=(ways[v]+ways[u])%mod;
+            for(int nd[]:list.get(node)){
+                int curr=nd[0];
+                int duri=nd[1];
+                if(dis+duri<dist[curr]){
+                    dist[curr]=dis+duri;
+                    count[curr]=count[node]%mod;
+                    pq.add(new long[]{dis+duri,curr});
+                }else if(dis+duri==dist[curr]){
+                    count[curr]=(count[curr]+count[node])%mod;
                 }
             }
         }
-        return ways[n-1];
+        return (int)count[n-1]%mod;
     }
 }
