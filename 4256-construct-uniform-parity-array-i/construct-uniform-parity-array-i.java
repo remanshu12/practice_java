27684541d9a9fbb8b69c44a19a2b07d8b1b46446
remanshu12 <remanshugoyal1@gmail.com>
2026-03-22 19:18:16 +0530

    Time: 1 ms (43.87%) | Memory: 45.3 MB (25.90%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int min=Integer.MAX_VALUE;
+        // for(int i:nums1){
+        //     if(i%2!=0)
+        //     min=Math.min(min,i);
+        // }
+        // int even=0;
+        // int odd=0;
+        // for(int i:nums1){
+        //     if(i%2==0) even++;
+        //     else odd++;
+        // }
+        // if(even==0 || odd==0) return true;
+        // // for(int i:nums1){
+        // //     if(i%2==0){
+        // //         if(i-min<1) return false;
+        // //     }
+        // // }
+        return true;
+
+    }
+}
