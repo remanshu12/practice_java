2601983b5247dc2de0657afa193e4907ab3c6aa1
remanshu12 <remanshugoyal1@gmail.com>
2026-03-24 00:41:46 +0530

    Time: 5 ms (49.82%) | Memory: 61.4 MB (84.81%) - LeetSync

--- 1659-get-the-maximum-score/get-the-maximum-score.java
+++ 1659-get-the-maximum-score/get-the-maximum-score.java
@@ -1,40 +1,31 @@
 class Solution {
     public int maxSum(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int n=nums1.length;
-        int m=nums2.length;
-        for(int i=0;i<m;i++){
-            map.put(nums2[i],i);
-        }
-        long mod=(long)1e9+7;
-        long prefix1[]=new long[n];
-        long prefix2[]=new long[m];
-        prefix1[0]=nums1[0];
-        for(int i=1;i<n;i++){
-            prefix1[i]=prefix1[i-1]+nums1[i];
-        }
-         prefix2[0]=nums2[0];
-        for(int i=1;i<m;i++){
-            prefix2[i]=prefix2[i-1]+nums2[i];
-        }
+        int i=0;
+        int j=0;
         long sum1=0;
         long sum2=0;
         long total=0;
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums1[i])){
-                int index=map.get(nums1[i]);
-                long temp1=prefix1[i]-sum1;
-                long temp2=prefix2[index]-sum2;
-                total+=Math.max(temp1,temp2);
-                sum1+=temp1;
-                sum2+=temp2;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                sum1+=nums1[i++];
+            }else if(nums1[i]>nums2[j]){
+                sum2+=nums2[j++];
+            }else{
+                total+=Math.max(sum1,sum2)+nums1[i];
+                i++;
+                j++;
+                sum1=0;
+                sum2=0;
             }
         }
-         long temp1=prefix1[n-1]-sum1;
-        long temp2=prefix2[m-1]-sum2;
-         total+=Math.max(temp1,temp2);
-       total=Math.max(total,prefix1[n-1]);
-       total=Math.max(total,prefix2[m-1]);
-       return (int)(total%mod);
+        while(i<nums1.length){
+            sum1+=nums1[i++];
+        }
+        while(j<nums2.length){
+            sum2+=nums2[j++];
+        }
+        long mod=(long)1e9+7;
+        total+=Math.max(sum1,sum2);
+        return (int)(total%mod);
     }
 }
