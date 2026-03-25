4c2e968f09e00dd11e4b4544ef76f9c318cf4eea
remanshu12 <remanshugoyal1@gmail.com>
2026-03-25 15:48:50 +0530

    Time: 48 ms (18.78%) | Memory: 99.4 MB (8.67%) - LeetSync

---
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        dp[0]=nums[0];
+       
+        int j=0;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
+         pq.add(new int[]{nums[0],0});
+        for(int i=1;i<n;i++){
+           
+            while(pq.peek()[1]<i-k){
+                int ll=pq.poll()[1];
+               // System.out.println(i+" "+ll);
+               // pq.poll();
+            }
+            int val=pq.peek()[0];
+            dp[i]=nums[i]+val;
+            pq.add(new int[]{dp[i],i});
+            
+        }
+    //   System.out.println(Arrays.toString(dp));
+        return dp[n-1];
+        
+    }
+}
