9290c93b5b149fe16b49314d27c2fc3551e2890d
remanshu12 <remanshugoyal1@gmail.com>
2026-03-31 00:03:10 +0530

    Time: 5 ms (11.42%) | Memory: 44.8 MB (8.91%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -10,14 +10,11 @@ class Solution {
             map.put(s.charAt(i),map.get(s.charAt(i))-1);
         if(visited[s.charAt(i)-'a']==1) continue;
             while(!st.isEmpty() && st.peek()>=s.charAt(i) && map.get(st.peek())>0){
-               
                 visited[st.peek()-'a']=0;
                 st.pop();
-            }
-             
+            } 
             st.push(s.charAt(i));
             visited[s.charAt(i)-'a']=1;
-
         }
         StringBuilder sb=new StringBuilder();
         while(!st.isEmpty()){
